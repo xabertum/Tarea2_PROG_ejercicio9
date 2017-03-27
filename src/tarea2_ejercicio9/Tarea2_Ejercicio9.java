d129f3ebdf2b83e9f900d518376212573e60bbd9
xabertum <mohatar@gmail.com>
2017-03-27 11:48:32 +0200

    first commit

---
+++ src/tarea2_ejercicio9/Tarea2_Ejercicio9.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2_ejercicio9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mohat
+ */
+public class Tarea2_Ejercicio9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int num, contador = 4;
+        int digitos[] = new int[4];
+
+        System.out.println("Introduce el numero: ");
+        num = scanner.nextInt();
+
+        while (num > 0) {
+            digitos[contador--] = num % 10;
+            num /= 10;
+
+        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print("El numero separado en digitos es: ");
+            System.out.println("" + digitos[i]);
+        }
+    }
+}
